e5b1e29361d321223081b5fc4010b7f3fce38d94
unknown <vinayak20injamure@gmail.com>
2024-05-30 22:15:28 +0530

    up

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,14 @@ import express, { json } from 'express';
 import cors from 'cors'; 
 const app=express();
 const port=process.env.port || 5000
-
+import path from 'path';
 const user={
     name:"strange.342"
 }
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 app.use(json())
 app.use(cors())
 app.get('/',(req,res)=>{
@@ -17,6 +21,12 @@ app.get('/signup',(req,res)=>{
 app.get('/dashboard',(req,res)=>{
     return res.json({message:"dashboard page"})
 })
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+
 app.listen(port,()=>{
     console.log("running")
 })
